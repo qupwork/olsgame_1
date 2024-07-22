84646b242f1430a5c5feed2934ec6674d70d203a
qdnqn <adnn.selimovic@gmail.com>
2024-07-22 10:37:36 +0200

    Fix db.js to parse environment variables correctly and change parameters for the MongoClient accordingly

--- db.js
+++ db.js
@@ -1,5 +1,7 @@
-const { MongoClient } = require('mongodb');
-require('dotenv').config(); // Ensure environment variables are loaded
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const path = require('path');
+require('dotenv').config({ path: path.join(__dirname, '.env') });
 
 const uri = process.env.MONGO_URI;
 
@@ -7,9 +9,18 @@ let db;
 
 async function connectToDatabase() {
     console.log('connectToDatabase called'); // Log function call
+    console.log(process.env.MONGO_URI)
     if (db) return db; // Return existing connection if already connected
     try {
-        const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+        const client = new MongoClient(uri, {
+                serverApi: {
+                    version: ServerApiVersion.v1,
+                    strict: true,
+                    deprecationErrors: true,
+                }
+            }
+        );
+
         await client.connect();
         console.log('Connected to MongoDB');
         db = client.db(); // Default database is used if not explicitly specified in the URI
@@ -20,4 +31,4 @@ async function connectToDatabase() {
     }
 }
 
-module.exports = connectToDatabase;
+module.exports = connectToDatabase
